d7bb242bb71e232e930d1b6bd6de590a598fcc2f
Funke Adebisi <mak_funke@yahoo.com>
2020-08-21 12:06:09 -0700

    Update loadjobs.js

--- loadjobs.js
+++ loadjobs.js
@@ -83,11 +83,11 @@ function sendData() {
 
    //create json payload that will be sent to api
    var application= {
-    "name":"Funke Adebisi",
-    "jobid":"5f3aea08ead4fc0001be475e",
-   "justification": "I have the skills and the experience required for the job position and I will be a great addition to your organization",
-   "code":"https://sourceforge.net/projects/sampleprojectpro/, https://github.com/AdebisiFunke/Job-Application, https://adebisifunke.github.io/Job-Application/",
-  "additionalLinks": "https://adebisifunke.github.io/Job-Application/listjobs.html"
+    name:"Funke Adebisi",
+    jobid:"5f3aea08ead4fc0001be475e",
+   justification: "I have the skills and the experience required for the job position and I will be a great addition to your organization",
+   code:"https://sourceforge.net/projects/sampleprojectpro/, https://github.com/AdebisiFunke/Job-Application, https://adebisifunke.github.io/Job-Application/",
+  additionalLinks: "https://adebisifunke.github.io/Job-Application/listjobs.html"
   }
 
    //create xhr object 
