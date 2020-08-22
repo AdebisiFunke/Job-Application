4617bee0c3c151217803f8ae7c6ececbd5c00a67
Funke Adebisi <mak_funke@yahoo.com>
2020-08-22 12:02:04 -0700

    Update loadjobs.js

--- loadjobs.js
+++ loadjobs.js
@@ -6,109 +6,103 @@
   * Author: Funke Adebisi
   */
 
-var job_url = "https://dev.spidasoftware.com/apply/jobs";
+//Create xmlhttp object that will be used to retreive data
+var xmlhttp = new XMLHttpRequest();
 
-//when page load excecute getJobData function
-window.onload = function() {
-  getJobData();
-};
+ //when page load excecute getJobData function
+ window.onload = function() {
+   getJobData();
+ };
+ 
+ //getJobData function will retrieve data from jason file on server and display it on listjobs.html page
+ function getJobData() {
+  var job_url,apply, apply;
+  //Declare variable that stores url location
+  job_url = "https://dev.spidasoftware.com/apply/jobs";
 
-//getJobData function will retrieve data from jason file on server and display it on listjobs.html page
-function getJobData() {
-
-  //Declare variables that stores values 
-  var apply = "Click Apply";
-
-  //Create xmlhttp object that will be used to retreive data
-  var xmlhttp = new XMLHttpRequest();
-
-  //check for state of request
-  xmlhttp.onreadystatechange = function() {
-
-    var store_job = "";
-
-    //check request status 
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-
-      var jobs = JSON.parse(xmlhttp.responseText);
-
-      //create required variables to store values, iterate through requirements and add value of each array elements to variables
-      for (var i = 0; i < jobs.length; i++) {
-        var JobID = jobs[i]._id;
-        var JobPos = jobs[i].position;
-        var JobDes = jobs[i].description;
-
-        //declare variable Req_lenght to store length of requirements array 
-        var Req_lenght = jobs[i].requirements.length;
-        var JobRequirements = "";
-        for (var j = 0; j < Req_lenght; j++) {
-
-          var store_requirements = jobs[i].requirements[j];
-          if (j == Req_lenght - 1) {
-            JobRequirements += store_requirements;
-          } else
-            JobRequirements += store_requirements + ",";
-
-        }
-        //create a container to store data 
-        store_job += "<div class='border p-5 bg-light'><p>" + "Job ID: " + JobID + "</p><p>" +
-          "Job Position: " + JobPos + "</p><p>" + "Job Description: " + JobDes + "</p><p>" + "Job Requirements: " + JobRequirements +
-          "</p><p>" + "<button class='btn btn-info' onclick='sendData()'>" + apply + "</button></p></div>";
-      }
-      //Assign the server response to HTML element with id Job, store data in store_job variable
-      document.getElementById("Job").innerHTML = store_job;
-    }
-  };
-
-  //initialize HTTP request method GET,  make request to job_url to retreive the JSON file data
-  //and make request asynchronous
-  xmlhttp.open("GET", job_url, true);
-
-  //send data
-  xmlhttp.send();
-
-}
-
-
-//function applyfor job will send json payload and store data into applications.json file on server
-function sendData() {
-  var application_url = "https://dev.spidasoftware.com/apply/applications";
-
-  //create json payload that will be sent to api
-  var application = {
-    name: "Funke Adebisi",
-    jobId: "5f3aea08ead4fc0001be475e",
-    justification: "I have the skills and the experience required for the job position and I will be a great addition to your organization",
-    code: "https://github.com/AdebisiFunke/Job-Application",
-    additionalLinks: ["https://sourceforge.net/projects/sampleprojectpro/", "https://adebisifunke.github.io/Job-Application/listjobs.html", "https://adebisifunke.github.io/precious-kids-preschool/", "https://adebisifunke.github.io/FORMPRO/"]
-  }
-
-  //create xhr object 
-  var xhr = new XMLHttpRequest();
-
-  //initialize HTTP request method  POST 
-  xhr.open("POST", application_url, true);
-
-  //set request header 
-  xhr.setRequestHeader("Content-Type", "application/json");
-
-  //create a state change call back
-  xhr.onreadystatechange = function() {
-
-    //check request status 
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      console.log("Application successfully sent");
-    } else
-      console.log("Unable to send application");
-
-  };
-
-  //convert json data to string
-  var data = JSON.stringify(application);
-
-  //print data to console
-  console.log(data);
-
-  //send data
-  xhr.send(data);
-}
+   //Declare variables that stores values 
+   apply = "Click Apply";
+ 
+   //check for state of request
+   xmlhttp.onreadystatechange = function() {
+   var jobs, store_job, JobID,JobPos,JobDes,Jobs,Req_lenght,JobRequirements, store_requirements;
+    store_job = "";
+ 
+     //check request status 
+     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+       jobs = JSON.parse(xmlhttp.responseText);
+ 
+       //create required variables to store values, iterate through requirements and add value of each array elements to variables
+       for (var i = 0; i < jobs.length; i++) {
+         JobID = jobs[i]._id;
+         JobPos = jobs[i].position;
+         JobDes = jobs[i].description;
+ 
+         //declare variable Req_lenght to store length of requirements array 
+         Req_lenght = jobs[i].requirements.length;
+         JobRequirements = "";
+         for (var j = 0; j < Req_lenght; j++) {
+ 
+           store_requirements = jobs[i].requirements[j];
+           if (j == Req_lenght - 1) {
+             JobRequirements += store_requirements;
+           } else
+             JobRequirements += store_requirements + ",";
+ 
+         }
+         //create a container to store data 
+         store_job += "<div class='border p-5 bg-light'><p>" + "Job ID: " + JobID + "</p><p>" +
+           "Job Position: " + JobPos + "</p><p>" + "Job Description: " + JobDes + "</p><p>" + "Job Requirements: " + JobRequirements +
+           "</p><p>" + "<button class='btn btn-info' onclick='sendData()'>" + apply + "</button></p></div>";
+       }
+       //Assign the server response to HTML element with id Job, store data in store_job variable
+       document.getElementById("Job").innerHTML = store_job;
+     }
+   };
+ 
+   //initialize HTTP request method GET,  make request to job_url to retreive the JSON file data
+   //and make request asynchronous
+   xmlhttp.open("GET", job_url, true);
+ 
+   //send data
+   xmlhttp.send();
+ 
+ }
+ 
+ 
+ //function applyfor job will send json payload and store data into applications.json file on server
+ function sendData() {
+  var application_url, data;
+  application_url = "https://dev.spidasoftware.com/apply/applications";
+ 
+   //create json payload that will be sent to api
+   application = {
+     name: "Funke Adebisi",
+     jobId: "5f3aea08ead4fc0001be475e",
+     justification: "I have the skills and the experience required for the job position and I will be a great addition to your organization",
+     code: "https://github.com/AdebisiFunke/Job-Application",
+     additionalLinks: ["https://sourceforge.net/projects/sampleprojectpro/", "https://adebisifunke.github.io/Job-Application/listjobs.html", "https://adebisifunke.github.io/precious-kids-preschool/", "https://adebisifunke.github.io/FORMPRO/"]
+   }
+   //initialize HTTP request method POST 
+    xmlhttp.open("POST", application_url, true);
+ 
+   //set request header 
+   xmlhttp.setRequestHeader("Content-Type", "application/json");
+ 
+   //create a state change call back
+   xmlhttp.onreadystatechange = function() {
+ 
+     //check request status 
+     if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+       console.log("Application successfully sent");
+     } else
+       console.log("Unable to send application");
+ 
+   };
+ 
+   //convert json data to string and store in it in data variable
+   data = JSON.stringify(application);
+ 
+   //send data
+   xmlhttp.send(data);
+ }
